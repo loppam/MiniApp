beda78570ec025395e385f34ef5bddce67f681e9
Daniel Pyrathon <pirosb3@gmail.com>
2025-06-03 17:26:30 -0400

    Revert "fix: update frame image URL and remove redundant URL property"
    This reverts commit e20d8afced611a8312209ed86f43470262fa4ae3.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ const appUrl = process.env.NEXT_PUBLIC_URL;
 
 const frame = {
   version: "next",
-  imageUrl: `https://farcaster.xyz/og-logo.png`,
+  imageUrl: `${appUrl}/opengraph-image`,
   button: {
     title: "Launch Frame",
     action: {
       type: "launch_frame",
       name: "Farcaster Frames v2 Demo",
+      url: appUrl,
       splashImageUrl: `${appUrl}/splash.png`,
       splashBackgroundColor: "#f7f7f7",
     },
