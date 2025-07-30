5047c1670265d9e64982abb1b00263b88d5f01d1
loppam <lolade132@gmail.com>
2025-07-30 22:41:08 +0100

    Big push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import "~/app/globals.css";
 import { Providers } from "~/app/providers";
 
 export const metadata: Metadata = {
-  title: "Farcaster Frames v2 Demo",
-  description: "A Farcaster Frames v2 demo app",
+  title: "Tradoor",
+  description: "Tradoor is an airdrop app.",
 };
 
 export default async function RootLayout({
--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from "next/og";
 
-export const alt = "Farcaster Frames V2 Demo";
+export const alt = "Tradoor";
 export const size = {
   width: 600,
   height: 400,
@@ -12,7 +12,7 @@ export default async function Image() {
   return new ImageResponse(
     (
       <div tw="h-full w-full flex flex-col justify-center items-center relative bg-white">
-        <h1 tw="text-6xl">Frames v2 Demo</h1>
+        <h1 tw="text-6xl">Tradoor</h1>
       </div>
     ),
     {
