333905ea2da73cec84419e41b92f78d09300a98f
loppam <lolade132@gmail.com>
2025-07-30 05:50:21 +0100

    Cheffings

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-import variants from 'tailwindcss-variants/plugin';
-
-export const content = ["./src/**/*.{js,ts,jsx,tsx}"];
-export const theme = {
-  extend: {},
-};
-export const plugins = [variants];
