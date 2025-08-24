47f46678e12f34267ec2534653766b94a2bf0df3
loppam <lolade132@gmail.com>
2025-08-24 22:13:31 +0100

    Big PUSH

--- src/app/frames/haptics/layout.tsx
+++ src/app/frames/haptics/layout.tsx
@@ -9,7 +9,7 @@ const frame = {
     action: {
       type: "launch_frame",
       name: "Farcaster Haptics Demo",
-      splashImageUrl: `https://frames-v2.vercel.app/splash.png`,
+      splashImageUrl: `https://mini-app-nine-ruddy.vercel.app/tradoor.png`,
       splashBackgroundColor: "#f7f7f7",
     },
   },
